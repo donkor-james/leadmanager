44812d163755bdfec22c721d53598f39689b6efc
Donkor James <jamesdonkor987@gmail.com>
2024-09-27 17:16:14 +0000

    first commit

---
+++ leadmanager/leads/urls.py
@@ -0,0 +1,7 @@
+from leads.api import LeadViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('api/leads', LeadViewSet, 'leads')
+
+urlpatterns = router.urls
